939d8511ae219226b090ceaee20fb0c86974e1d9
엽킹 <kim91star2@naver.com>
2022-11-21 23:21:35 +0900

    11

---
+++ src/main/java/com/yeopking/springwithaws/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.yeopking.springwithaws.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
---
+++ src/test/java/com/yeopking/springwithaws/SpringWithAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeopking.springwithaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
